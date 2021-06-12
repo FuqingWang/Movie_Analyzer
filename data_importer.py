4d01197309a2ebe11991df50b11d911b5085d1f8
FuqingWang <574594544@qq.com>
2021-06-12 01:22:14 -0400

    add test cases

--- data_importer.py
+++ data_importer.py
@@ -6,6 +6,10 @@ cur = conn.cursor()
 print("connected to sqlite3")
 
 
+def db_drop_table():
+    cur.execute("DROP TABLE IF EXISTS movie_table;")
+    print("Table cleared")
+
 def db_create_table():
     cur.execute("CREATE TABLE IF NOT EXISTS movie_table ("
                 "color, director_name, num_critic_for_reviews, duration, director_facebook_likes, "
@@ -30,16 +34,16 @@ def db_import_csv():
                              (str(line[0]), str(line[1]), str(line[2]), str(line[3]), str(line[4]), str(line[5]),
                               str(line[6]), str(line[7]), float(line[8]), str(line[9]), str(line[10]), str(line[11]),
                               str(line[12]), str(line[13]), str(line[14]), str(line[15]), str(line[16]), str(line[17]),
-                              str(line[18]), str(line[19]), str(line[20]), str(line[21]), float(line[22]), str(line[23]),
+                              str(line[18]), str(line[19]), str(line[20]), str(line[21]), float(line[22]),
+                              str(line[23]),
                               str(line[24]), str(line[25]), str(line[26]), str(line[27])))
-            if line[9] == 'Family|Sci-Fi':
-                print(f"gross: {line[8]}, budget: {line[22]}")
-                print(line[8] != '' and line[22] != '')
+
     print("Data imported successfully")
 
 
+db_drop_table()
 db_create_table()
 db_import_csv()
 conn.commit()
 conn.close()
-print("close connection")
+print("close connection to sqlite3")
--- unit_test.py
+++ unit_test.py
@@ -1,34 +1,39 @@
 import main
 import sqlite3
+import unittest
 
 conn = sqlite3.connect('movie.db')
 
 
-def test_top10_profit_by_generes():
-    actual_output = main.get_top10_category_by_profit('genres')
-    expect_output = []
-    sql_result = conn.execute("SELECT genres, AVG(profit) AS avg_profit "
-                              "FROM (SELECT genres, (gross - budget) AS profit "
-                              "      FROM movie_table) AS profit_summary "
-                              "GROUP BY genres "
-                              "ORDER BY avg_profit DESC "
-                              "LIMIT 10;")
-    for row in sql_result:
-        expect_output.append(row[0])
+class Test(unittest.TestCase):
 
-    assert actual_output == expect_output
+    def test_top10_profit_by_generes(self):
+        actual_output = main.Main.get_top10_category_by_profit('genres')
+        expect_output = []
+        sql_result = conn.execute("SELECT genres, AVG(profit) AS avg_profit "
+                                  "FROM (SELECT genres, (gross - budget) AS profit "
+                                  "      FROM movie_table) AS profit_summary "
+                                  "GROUP BY genres "
+                                  "ORDER BY avg_profit DESC "
+                                  "LIMIT 10;")
+        for row in sql_result:
+            expect_output.append(row[0])
+        assert actual_output == expect_output, f"\nactual_output is {actual_output}, \nexpect_output is {expect_output}"
+
+    def test_top10_profit_by_director(self):
+        actual_output = main.Main.get_top10_category_by_profit('director_name')
+        expect_output = []
+        sql_result = conn.execute("SELECT director_name, AVG(profit) AS avg_profit "
+                                  "FROM (SELECT director_name, (gross - budget) AS profit "
+                                  "      FROM movie_table) AS profit_summary "
+                                  "GROUP BY director_name "
+                                  "ORDER BY avg_profit DESC "
+                                  "LIMIT 10;")
+        for row in sql_result:
+            expect_output.append(row[0])
+
+        assert actual_output == expect_output, f"\nactual_output is {actual_output}, \nexpect_output is {expect_output}"
 
 
-def test_top10_profit_by_director():
-    actual_output = main.get_top10_category_by_profit('director_name')
-    expect_output = []
-    sql_result = conn.execute("SELECT director_name, AVG(profit) AS avg_profit "
-                              "FROM (SELECT director_name, (gross - budget) AS profit "
-                              "      FROM movie_table) AS profit_summary "
-                              "GROUP BY director_name "
-                              "ORDER BY avg_profit DESC "
-                              "LIMIT 10;")
-    for row in sql_result:
-        expect_output.append(row[0])
-
-    assert actual_output == expect_output
+if __name__ == '__main__':
+    unittest.main()
